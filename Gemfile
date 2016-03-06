a3101192b5312321a1598cd2ebf1f190d8e1cb16
Patrick Yip <patrickyip2006@gmail.com>
2016-03-06 13:09:28 +0800

    Add Paginate

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'bootstrap-datepicker-rails'
 gem 'devise'
+gem 'will_paginate', '~> 3.0.5'
+gem 'will_paginate-bootstrap'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,7 +3,7 @@ class LogsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @logs = Log.all.order("created_at DESC")
+    @logs = Log.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
   end
 
   def show
