59712d6443667d130ac88a2eac042b801eb2a2ee
Patrick Yip <patrickyip2006@gmail.com>
2016-03-22 23:36:25 +0800

    a little change for authorization

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,7 @@
 class LogsController < ApplicationController
   before_action :set_log, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :authorize_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @q = Log.ransack(params[:q])
@@ -19,65 +20,46 @@ class LogsController < ApplicationController
   def show
   end
 
-  def new
-    if current_user.viewer?
-      redirect_to root_path
-    else
-      @log = current_user.logs.build
-      @log.prepared = current_user.name
-    end
+  def new    
+    @log = current_user.logs.build
+    @log.prepared = current_user.name
   end
 
   def edit
-    if current_user.viewer?
-      redirect_to root_path
-    end
   end
 
   def create
-    if current_user.viewer?
-      redirect_to root_path
-    else
-      @log = current_user.logs.build(log_params)
-      @log.prepared = current_user.name
+    @log = current_user.logs.build(log_params)
+    @log.prepared = current_user.name
 
-      respond_to do |format|
-        if @log.save
-          format.html { redirect_to root_path, notice: 'Record was successfully created.' }
-          format.json { render :show, status: :created, location: @log }
-        else
-          format.html { render :new }
-          format.json { render json: @log.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @log.save
+        format.html { redirect_to root_path, notice: 'Record was successfully created.' }
+        format.json { render :show, status: :created, location: @log }
+      else
+        format.html { render :new }
+        format.json { render json: @log.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
-    if current_user.viewer?
-      redirect_to root_path
-    else
-      respond_to do |format|
-        if @log.update(log_params)
-          format.html { redirect_to @log, notice: 'Record was successfully updated.' }
-          format.json { render :show, status: :ok, location: @log }
-        else
-          format.html { render :edit }
-          format.json { render json: @log.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @log.update(log_params)
+        format.html { redirect_to @log, notice: 'Record was successfully updated.' }
+        format.json { render :show, status: :ok, location: @log }
+      else
+        format.html { render :edit }
+        format.json { render json: @log.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    if current_user.viewer?
-      redirect_to root_path
-    else
-      @log.destroy
-      respond_to do |format|
-        format.html { redirect_to logs_url, notice: 'Record was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+    @log.destroy
+    respond_to do |format|
+      format.html { redirect_to logs_url, notice: 'Record was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
@@ -87,11 +69,19 @@ class LogsController < ApplicationController
   end
 
   private
-    def set_log
-      @log = Log.find(params[:id])
-    end
+  
+  def set_log
+    @log = Log.find(params[:id])
+  end
 
-    def log_params
-      params.require(:log).permit(:chq_number, :chq_date, :payee_name, :category, :deal_id, :particular, :prepared, :currencies, :amount, :sign_date, :present_date, :status, :salesperson, :voucher_no, :void_reason)
+  def log_params
+    params.require(:log).permit(:chq_number, :chq_date, :payee_name, :category, :deal_id, :particular, :prepared, :currencies, :amount, :sign_date, :present_date, :status, :salesperson, :voucher_no, :void_reason)
+  end
+
+  def authorize_user!
+    if current_user.viewer?
+      flash[:alert] = 'You are not allowed to access this page.'
+      redirect_to root_path
     end
+  end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -27,3 +27,5 @@ class Log < ActiveRecord::Base
 
 
 end
+
+
